4b803418ee4b782ba4511ecfb3c70567858e544a
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-04-01 11:44:19 +0200

    Can now randomly choose a light source.

--- rt/src/rt/LightList.java
+++ rt/src/rt/LightList.java
@@ -1,9 +1,16 @@
 package rt;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * A list of light sources.
  */
 public class LightList extends ArrayList<LightGeometry> {
+	
+	public LightGeometry getRandomLightSource(){
+		int randomIndex = new Random().nextInt(this.size());
+		
+		return this.get(randomIndex);
+	}
 }
